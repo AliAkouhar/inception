67780a3c424ecbb57591ff43bc341f4c396c8128
AliAkouhar <akouhar04ali@gmail.com>
2025-08-27 10:51:55 +0100

    puah

--- src/requirements/mariadb/tools/mariadb.sh
+++ src/requirements/mariadb/tools/mariadb.sh
@@ -5,5 +5,13 @@ service mariadb start
 sleep 5
 
 mysql -u root -p <<EOF
-    CREATE DATABASE 
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\';
+CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
 EOF
+
+mysqladmin -u root -p "${MYSQL_ROOT_PASSWORD}" shutdown
+
+exec mysqld_safe
